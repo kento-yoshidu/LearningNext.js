f738cde9386dd4f9bf60d9e46e81f6d4b4f9f341
kento <kento@toriwatari.bird>
2021-04-18 19:13:45 +0900

    Update P49

--- dev2/app.js
+++ dev2/app.js
@@ -3,6 +3,8 @@ Vue.createApp({
     return {
       todoTitle: "", //入力されたtodoのタイトル
       todoDescription: "", //入力されたdescription
+      todoCategories: [], //検索対象として選択されたカテゴリ
+      hideDOneTodo: false, //終了したものも表示させるかの選択
       searchWord: "", //入力された検索ワード
     }
   }
