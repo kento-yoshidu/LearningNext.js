407b81a3cc069414fc6bfdde222ba4e88e56b886
kento <kento@toriwatari.bird>
2021-04-18 19:50:19 +0900

    Update P54

--- dev2/app.js
+++ dev2/app.js
@@ -3,9 +3,11 @@ Vue.createApp({
     return {
       todoTitle: "", //入力されたtodoのタイトル
       todoDescription: "", //入力されたdescription
-      todoCategories: [], //検索対象として選択されたカテゴリ
+      todoCategories: [], //todoのカテゴリ
+      selectedCategory: " ", //検索するカテゴリ
       hideDOneTodo: false, //終了したものも表示させるかの選択
       searchWord: "", //入力された検索ワード
+      order: "desc", //todoの表示順
     }
   }
 }).mount("#app")
--- sample/p41/app.js
+++ sample/p41/app.js
@@ -1,7 +1,7 @@
 Vue.createApp({
   data: function() {
     return {
-      todoCategories: [],
+      message: "Message"
     }
-  },
-}).mount("#app");
+  }
+}).mount("#app")
