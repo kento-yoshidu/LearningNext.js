d704595303172692a59e4849a834132f141c5f1c
kento <kento@toriwatari.bird>
2021-04-24 20:33:54 +0900

    Update P130

--- sample/p124/app.js
+++ sample/p124/app.js
@@ -1,14 +1,20 @@
-const countUpButton = {
-  template: "#btn-template"
+const countButton = {
+  template: '#btn-template'
+}
+
+const countButton2 = {
+  template: '#btn-template2'
 }
 
 Vue.createApp({
   data: function() {
     return {
       count: 0,
+      count2: 100
     }
   },
   components: {
-    'count-up-button': countUpButton
+    'count-up-button': countButton,
+    'count-up-button2': countButton2
   }
-}).mount("#app")
+}).mount('#app')
