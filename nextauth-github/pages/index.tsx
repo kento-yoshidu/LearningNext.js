532387f5a5c1f9f7612291d60c313a229e36160f
kento <kento@toriwatari.bird>
2022-07-31 11:52:36 +0900

    ✨ Update

--- nextauth-github/pages/index.tsx
+++ nextauth-github/pages/index.tsx
@@ -1,28 +1,33 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+
+import Link from "next/link"
 
 import { useSession, signIn, signOut } from "next-auth/react"
 
 const Home: NextPage = () => {
   const { data: session } = useSession()
 
-  if (session) {
-    return (
-      <>
-        Signed in as {session?.user?.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    )
-  }
-
   return (
     <>
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
+      <h1>Next-auth テストサイト</h1>
+
+      {session ? (
+        <>
+          <h2>{session?.user?.name}さんでログインしています。</h2>
+
+          <p><Link href="/item">アイテム一覧を見る</Link></p>
+          <button onClick={() => signOut()}>ログアウトする</button>
+        </>
+      ) : (
+        <>
+          <h2>まだログインしていません。</h2>
+
+          <button onClick={() => signIn()}>Sign in</button>
+          <p><Link href="/item">アイテム一覧を見る</Link></p>
+        </>
+      )}
     </>
-  ) 
+  )
 }
 
 export default Home
