9c40bb622df8624446e96ff7dc41ddf0475fbf65
kento <kento@toriwatari.bird>
2022-07-31 11:52:36 +0900

    ✨ Update

--- nextauth-github/pages/api/auth/[...nextauth].ts
+++ nextauth-github/pages/api/auth/[...nextauth].ts
@@ -5,8 +5,13 @@ export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
+      /* @ts-ignore */
       clientId: process.env.GITHUB_ID,
+      /* @ts-ignore */
       clientSecret: process.env.GITHUB_SECRET,
     })
-  ]
+  ],
+  pages: {
+    signIn: "/auth/signin"
+  }
 })
