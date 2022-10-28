a9c6721bf34f0439712be93e0543460e83b3959c
kento <kento@toriwatari.bird>
2022-10-28 21:38:37 +0900

    ✨ Update

--- react-hook-form/pages/index.tsx
+++ react-hook-form/pages/index.tsx
@@ -1,8 +1,13 @@
+import Link from "next/link"
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <h1>React-hook-form</h1>
+    <>
+      <h1>React-hook-form</h1>
+
+      <Link href="/post">Post</Link>
+    </>
   )
 }
 
